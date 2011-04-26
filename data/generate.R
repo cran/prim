ff55de3c0a7b477a224410dd2260323462917a3e
Tarn Duong <tarn.duong@gmail.com>
2011-04-26 00:00:00 +0000

    version 1.0.10

--- data/generate.R
+++
@@ -1,9 +0,0 @@
-
-if (0)
-{
-quasiflow <- read.table("hiv-scaled.dat", header=TRUE)
-quasiflow[,4] <- 2*quasiflow[,4]-1
-colnames(quasiflow) <- c("x1", "x2", "x3", "y")
-save(quasiflow, file="quasiflow.RData", ascii=TRUE, version=2)
-}
-
